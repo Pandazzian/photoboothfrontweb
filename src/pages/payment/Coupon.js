791db6eaadfaf9cc7398e402c487fa0e96afce64
Pandazzian <30854497+Pandazzian@users.noreply.github.com>
2023-09-25 16:43:15 +0700

    the coupons works now still need ux/ui tweeking tho cuz I cant get swal to work properly therefore I commented it and use alert() instead bad UI i know but it works. what you gonna do about it

--- src/pages/payment/Coupon.js
+++ src/pages/payment/Coupon.js
@@ -1,13 +1,23 @@
 
 import '../../App.css';
 import React from 'react';
-import { Link, redirect } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { urls } from '../../app/api';
-import { Image } from 'react-bootstrap';
-import { useState,useEffect } from "react"
+import { Image,Form,Spinner } from 'react-bootstrap';
+import { useState } from "react"
+// import {Swal} from 'sweetalert2';
+// import withReactContent from 'sweetalert2-react-content'
+
+
 
 export function Coupon({pathurl}) {
+    const navigate = useNavigate();
+    // const MySwal = withReactContent(Swal)
+
+    const API_URL = urls.gql;
+
+    const API_KEY = urls.apiSecretKey;
 
     let bgurl = pathurl+"coupon_bg.png"
 
@@ -15,30 +25,172 @@ export function Coupon({pathurl}) {
 
     let buttonNexturl = pathurl+"payment_next_coupon.png"
 
-    const verifyCoupon = async () => {
-        redirect("/choosephotoframe")
+    const [field,setField] = useState("")
+    const [loading,setLoading] = useState(false)
+
+    const pressNum = (num) => {
+        setField(field+num)
+    }
+    const pressBack = () => {
+        if(field===""){
+            navigate("/payment")
+        }
+        else{
+            setField(field.slice(0, -1))
+        }
     }
 
+    const verifyCoupon = async () => {
+        setLoading(true)
+        const query = `
+        query MyQuery {
+            listCoupons(filter: {Code: {eq: "${field}"}, isValid: {eq: true}}) {
+              nextToken
+              items {
+                Code
+                createdAt
+                expDate
+                id
+                isValid
+                updatedAt
+              }
+            }
+          }
+        `;
+
+        // Set up Axios config with the "x-api-key" header
+        const axiosConfig = {
+        headers: {
+            'x-api-key': API_KEY,
+            'Content-Type': 'application/json',
+        },
+        };
+
+        // Make the GraphQL API request
+        axios
+        .post(API_URL, { query }, axiosConfig)
+        .then((response) => {
+            // setData(response.data);
+            let couponlist = response.data.data.listCoupons.items
+            // console.log(couponlist.items.length);
+            if(couponlist.length === 0){
+                setLoading(false)
+                // MySwal.fire({
+                //     title: <strong>Oops!</strong>,
+                //     html: <i>Invalid code plaease try again</i>,
+                //     icon: 'warning'
+                // })
+                alert("invalid code plaease try again")
+            }else{
+                setInvalid(couponlist[0])
+            }
+        })
+        .catch((error) => {
+            setLoading(false)
+            // MySwal.fire({
+            //     title: <strong>An error occur.</strong>,
+            //     html: <i>{error} please try again</i>,
+            //     icon: 'error'
+            // })
+            alert("an Error occor pleasr try again")
+            console.error('Error:', error);
+        });
+        // redirect("/choosephotoframe")
+    }
+
+    const setInvalid = async (coupon) => {
+        const query = `
+        mutation MyMutation {
+            updateCoupon(input: {id: "${coupon.id}", isValid: false}) {
+              id
+            }
+          }
+        `;
+
+        // Set up Axios config with the "x-api-key" header
+        const axiosConfig = {
+        headers: {
+            'x-api-key': API_KEY,
+            'Content-Type': 'application/json',
+        },
+        };
+
+        // Make the GraphQL API request
+        axios
+        .post(API_URL, { query }, axiosConfig)
+        .then((response) => {
+            console.log(response.data.data.updateCoupon.id===coupon.id);
+            if(response.data.data.updateCoupon.id===coupon.id){
+                navigate("/choosephotoframe")
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+
+
     return (
-        <div className="background-container" style={{backgroundImage:'url("'+bgurl+'")', alignItems: "end",textAlign:'center'}}>
+        <div className="background-container" style={{backgroundImage:'url("'+bgurl+'")', display:"inherit",textAlign: "-webkit-center",paddingTop:'400px'}}>
             <div className='row'>
-                {/* <div className='ml-auto col-12 mr-auto' style={{marginBottom: "10rem"}}>
-                    <Link to={paymentUrl} style={{marginBotto: '10rem'}}>
-                        <Image src={paybuttonurl} rounded />
-                    </Link> 
+                <div className='col-2'/>
+                <div className='col-8'>
+                    <Form.Control type="text" placeholder="" value={field} readOnly size={'lg'} style={{height:"120px",backgroundColor:"orange",fontSize:'50px',color:'white',textAlign: "-webkit-center"}}/>
                 </div>
-                <div className='ml-auto col-12 mr-auto' style={{marginBottom: "20rem"}}>
-                    <Link to={paymentUrl} style={{marginBotto: '10rem'}}>
-                        <Image src={couponurl} rounded />
-                    </Link> 
-                </div> */}
+            </div>
+            <div className='row' style={{marginTop:'2rem'}}>
+                <div className='col-4'>
+                    <Image src={pathurl+"1.png"} onClick={()=>pressNum("1")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"2.png"} onClick={()=>pressNum("2")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"3.png"} onClick={()=>pressNum("3")} rounded />
+                </div>
+            </div>
+            <div className='row' style={{marginTop:'2rem'}}>
+                <div className='col-4'>
+                    <Image src={pathurl+"4.png"} onClick={()=>pressNum("4")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"5.png"} onClick={()=>pressNum("5")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"6.png"} onClick={()=>pressNum("6")} rounded />
+                </div>
+            </div>
+            <div className='row' style={{marginTop:'2rem'}}>
+                <div className='col-4'>
+                    <Image src={pathurl+"7.png"} onClick={()=>pressNum("7")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"8.png"} onClick={()=>pressNum("8")} rounded />
+                </div>
+                <div className='col-4'>
+                    <Image src={pathurl+"9.png"} onClick={()=>pressNum("9")} rounded />
+                </div>
+            </div>
+            <div className='row' style={{marginTop:'2rem'}}>
+                <div className='col-4'/>
+                <div className='col-4'>
+                    <Image src={pathurl+"0.png"} onClick={()=>pressNum("0")} rounded />
+                </div>
+            </div>
+            <div className='row' style={{marginTop:'2rem'}}>
                 <div style={{marginBottom: "5rem"}}>
-                    <Link to='/payment'>
-                        <Image src={buttonBackurl} rounded />
-                    </Link>
-                    <Link to='/choosephotoframe'>
-                        <Image src={buttonNexturl} rounded />
-                    </Link>
+                { loading?
+                    <Spinner animation="border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                    :
+                    <div>
+                        <Image src={buttonBackurl} onClick={()=>pressBack()} rounded />
+                        {/* <Link to='/choosephotoframe'> */}
+                        <Image src={buttonNexturl} onClick={()=>verifyCoupon()} rounded />
+                        {/* </Link> */}
+                    </div>
+                }
                 </div>
             </div>
         </div>
