ad7a245527a6ca1737e694725746c332215a7432
Pandazzian <30854497+Pandazzian@users.noreply.github.com>
2023-10-06 16:52:52 +0700

    I done fucked up so ummm I used unity and then abandoned this project cus... React SUCKS BALLS!!! ... eh actually im just noob at it so kinda my fault kinda not u know stuff happens anyways BYE!!

--- src/app/api.js
+++ src/app/api.js
@@ -1,5 +1,6 @@
 export const urls = {
     payment : 'https://api.payplug.com/v1/payments',
     gql : 'https://wnoac7qyhveyfag42cigzz5xp4.appsync-api.ap-southeast-2.amazonaws.com/graphql',
-    apiSecretKey: 'da2-eh73rk5sznc7dmhy7xrw53ws2u'
+    apiSecretKey: 'da2-eh73rk5sznc7dmhy7xrw53ws2u',
+    aws: 'https://iexb69www5.execute-api.ap-southeast-2.amazonaws.com/dev'
 } 
--- src/pages/filter/ChooseFilter.js
+++ src/pages/filter/ChooseFilter.js
@@ -5,19 +5,66 @@ import { Link } from 'react-router-dom';
 import { Image,Carousel,Stack,Card } from 'react-bootstrap';
 import { useState,useEffect } from "react"
 import './filter.css';
+import { useDispatch,useSelector } from 'react-redux';
+import axios from 'axios';
+import { urls } from '../../app/api';
 
 export function ChooseFilter({pathurl}) {
-    const [filterList, setFilterList] = useState([pathurl+"filter1.png", pathurl+"filter2.png", pathurl+"filter3.png"]);
+    const [filterList, setFilterList] = useState([
+        {
+            img: pathurl+"filter1.png",
+        },
+        {
+            img: pathurl+"filter2.png",
+        },
+        {
+            img: pathurl+"filter3.png",
+        }
+    ]);
     let bgurl = pathurl+"filter_bg.png"
     let nextButtonUrl = pathurl+"filter_print.png"
 
-    // useEffect(() => {
-    //     getFilterList()
-    // });
+    const capturedImages = useSelector((state) => state.Images);
+    // capturedImages.images[0].payload
+    const getImageFilter = async () => {
+        // const encodedParams = new URLSearchParams();
+        // encodedParams.set('q', 'English is hard, but detectably so');
 
-    // const getFilterList = () => {
-    //     setFilterList([pathurl+"filter1.png", pathurl+"filter2.png", pathurl+"filter3.png"])
-    // }
+        // const options = {
+        // method: 'GET',
+        // url: 'https://restcountries.com/v3.1/independent',
+        // headers: {
+        //     'status': 'true'
+        // },
+        // data: encodedParams,
+        // };
+
+        // try {
+        //     const response = await axios.request(options);
+        //     console.log(response.data);
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    const query = {
+        'image':capturedImages.images[0].payload
+    }
+    const axiosConfig = {
+        headers: {
+            // 'x-api-key': API_KEY,
+            'Content-Type': 'application/json',
+        },
+        };
+    axios.post(urls.aws,query,axiosConfig).then((res) => {
+                console.log("RESPONSE RECEIVED: ", res);
+            }).catch(()=>{
+                console.log("catched")
+            })
+    }
+
+    useEffect(() => {
+        // Define your GraphQL query
+        getImageFilter();
+    }, []);
 
     return (
         <div className="background-container" style={{backgroundImage:'url("'+bgurl+'")',textAlign:"center"}}>
@@ -27,7 +74,13 @@ export function ChooseFilter({pathurl}) {
                     interval={5000}
                     controls={true}
                     indicators={false}
-                 >
+                >
+                    {filterList.map((filter)=>{
+                        <Carousel.Item className='col-12' style={{textAlignLast:"center"}}>
+                            <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filter.img} rounded /></Card> 
+                        </Carousel.Item>
+                    })}
+{/*                     
                     <Carousel.Item className='col-12' style={{textAlignLast:"center"}}>
                                 <Stack
                                     direction="horizontal"
@@ -38,47 +91,10 @@ export function ChooseFilter({pathurl}) {
                                             <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filterList[1]} rounded /></Card>
                                             <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filterList[2]} rounded /></Card>    
                                 </Stack>
-                    </Carousel.Item>
-                    <Carousel.Item className='col-12' style={{textAlignLast:"center"}}>
-                                <Stack
-                                    direction="horizontal"
-                                    className="h-100 justify-content-center align-items-center"
-                                    gap={3}
-                                        >
-                                            <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filterList[0]} rounded /></Card>
-                                            <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filterList[1]} rounded /></Card>
-                                            <Card style={{width:"200px", height:"200px", borderColor:'transparent', backgroundColor:'transparent'}} ><Image style={{width:"200px"}} src={filterList[2]} rounded /></Card>    
-                                </Stack>
-                    </Carousel.Item>
-                    {
-                        
-                    }
-                    {/* <Carousel.Item className='col-12' style={{textAlignLast:"center"}}>
-                        <Image style={{width:"300px"}} src={pathurl+"filter1.png"} rounded />    
-                        <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item style={{textAlignLast:"center"}}>
-                    <Image style={{width:"400px"}} src={pathurl+"filter2.png"} rounded />
-                        <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item style={{textAlignLast:"center"}}>
-                    <Image style={{width:"400px"}} src={pathurl+"filter3.png"} rounded />
-                        <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                        </Carousel.Caption>
-                    </Carousel.Item>*/}
+                    </Carousel.Item> */}
                 </Carousel>
                 <div className='col-12 mb-3 mt-3'>
-                    <Image className='col-12' src={pathurl+"preview.png"} rounded style={{width:"400px"}} />
+                    <Image className='col-12' src={capturedImages.images[0].payload} rounded style={{width:"400px"}} />
                 </div>
                 {/* <Image className='ml-auto mr-auto col-12' src={pathurl+"preview.png"} rounded width={300}/> */}
                 <div className='ml-auto col-12 mr-auto mt-5' >
